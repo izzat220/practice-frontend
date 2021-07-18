dbabb27865f09dc512562fb66f6ae9f38569110b
izzat alolwani <izzat.alolwani@gmail.com>
2021-07-18 12:28:22 +0400

    update examples

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,34 +4,76 @@
 
 	let users;
 
-	onMount(async () => {
-		let response = await axios.get("http://localhost:8080/getStuff");
-		users = response.data.results;
+	let rowsToEdit = [];
+
+	const getUsers = async () => {
+		let response = await axios.get("http://localhost:8080/getUsers");
+		users = response.data;
 		console.log(users);
+	};
+
+	const editRow = (i) => {
+		if (rowsToEdit.includes(i)) {
+			let index = rowsToEdit.indexOf(i);
+			rowsToEdit.splice(index, 1);
+		} else {
+			rowsToEdit.push(i);
+		}
+
+		rowsToEdit = rowsToEdit;
+	};
+
+	onMount(() => {
+		getUsers();
 	});
+
+	const save = async (newFirstName, userId) => {
+		console.log(users);
+		let response = await axios.post("http://localhost:8080/updateUser", {
+			userId: userId,
+			newFirstName: newFirstName,
+		});
+
+		console.log(response);
+		getUsers();
+	};
 </script>
 
-{#if users}
-	<table class="table">
-		<thead>
-			<tr>
-				<th scope="col">FirstName</th>
-				<th scope="col">LastName</th>
-				<th scope="col">Age</th>
-			</tr>
-		</thead>
-		<tbody>
+<table class="table">
+	<thead>
+		<tr>
+			<th scope="col">FirstName</th>
+			<th />
+			<th />
+		</tr>
+	</thead>
+	<tbody>
+		{#if users}
 			{#each users as user, i}
 				<tr>
-					<td>{user.firstName}</td>
-					<td>{user.lastName}</td>
-					<td>{user.age}</td>
+					<td>
+						{#if rowsToEdit.includes(i)}
+							<input
+								class="form-control form-control-sm"
+								bind:value={user.firstName}
+							/>
+						{:else}
+							{user.firstName}
+						{/if}
+					</td>
+					<td
+						><button class="btn btn-light btn-sm" on:click={() => editRow(i)}
+							>Edit</button
+						></td
+					>
+					<td
+						><button
+							class="btn btn-primary btn-sm"
+							on:click={() => save(user.firstName, user._id)}>Save</button
+						></td
+					>
 				</tr>
 			{/each}
-		</tbody>
-	</table>
-{:else}
-	<div class="spinner-border" role="status">
-		<span class="visually-hidden">Loading...</span>
-	</div>
-{/if}
+		{/if}
+	</tbody>
+</table>
